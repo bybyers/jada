45052157ce43db25c526ff14ed7322c564ad6b79
byebyers <byers.jacob@gmail.com>
2024-07-11 16:36:13 -0600

    sticky header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,6 @@
 // Tools
 import Image from "next/image"
 import Link from "next/link"
-import { SanityDocument } from "next-sanity"
 
 // Types
 import { NavigationType } from "@/types/documents/navigation-type"
@@ -16,20 +15,21 @@ interface HeaderProps {
   items: NavigationType
 }
 
-
 const Header: React.FC<HeaderProps> = ({
   items
 }) => {
 
   return (
-    <header className='w-full flex justify-center pt-10 pb-16'>
+    <header 
+      className='sticky top-0 z-50 w-full flex justify-center pt-10 pb-4 mb-12 bg-background'
+    >
       <div className={`flex ${items ? 'flex-col gap-y-5 lg:gap-y-10  items-center justify-center lg:justify-between pb-10 lg:pb-0 lg:pr-10' : 'justify-center'} w-full max-w-6xl xl:max-w-7xl 2xl:max-w-8xl`}>
         <Link href='/' className='w-24 lg:w-auto'>
           <Image src={Logo} alt='Logo' width={190} height={190} />
         </Link>
-       {items && (
-         <Nav data={items} />
-       )}
+        {items && (
+          <Nav data={items} />
+        )}
       </div>
     </header>
   )
