2ef9c8c5da68052c53b812ed3ba5a2e68bb135e5
byebyers <byers.jacob@gmail.com>
2024-07-12 15:52:30 -0600

    review component preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,6 +11,7 @@ import { media } from 'sanity-plugin-media'
 import { deskStructure } from '@/sanity/structure'
 import SpotlightLogo from '@/components/logo'
 import { locate } from '@/sanity/presentation/locate'
+import { defaultDocumentNode } from '@/sanity/lib/defaultDocumentNode'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -44,7 +45,8 @@ export default defineConfig({
   },
   plugins: [
     structureTool({
-			structure: deskStructure
+			structure: deskStructure,
+      defaultDocumentNode
 		}),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
