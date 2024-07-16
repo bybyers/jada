13ad4ddba2792d659155f7fc0d5e8b0098c16dba
byebyers <byers.jacob@gmail.com>
2024-07-16 14:38:38 -0600

    pice column justify

--- components/price-block/index.tsx
+++ components/price-block/index.tsx
@@ -79,43 +79,47 @@ const PriceBlock: React.FC<PriceBlockType> = ({
 
               return (
                 <div key={index} className='w-full max-w-md  px-5 md:px-16 py-5 md:py-10 first:rounded-tl-3xl md:first:rounded-bl-3xl first:rounded-tr-3xl md:first:rounded-tr-none last:rounded-tr-none md:last:rounded-tr-3xl last:rounded-br-3xl last:rounded-bl-3xl md:last:rounded-bl-none md:last:rounded-bl-0 border-[#c8a7f2]  first:border-t-2 md:border-t-2 border-b-2 first:border-r-2 md:first:border-r-0 border-l-2 last:border-r-2 md:last:border-r-2 shadow shadow-white'>
-                  <div className='w-full flex flex-col items-center gap-y-2'>
-                    <div className='space-y-5'>
-                      {column.title && (<p className={`text-3xl font-medium bg-gradient-to-b ${last ? 'from-[#9c83fe]' : 'from-[#d6d5d5]'} ${last ? 'to-[#f6f5f5]' : 'to-white'} text-transparent bg-clip-text`}>{column.title}</p>)}
-                      {column.price && (<h2 className={`text-7xl font-semibold bg-gradient-to-b ${last ? 'from-[#9c83fe]' : 'from-[#d6d5d5]'} ${last ? 'to-[#f6f5f5]' : 'to-white'} ${inter.className} text-transparent bg-clip-text`}>${column.price}</h2>)}
-                      {column.offer && (<p>{column.offer}</p>)}
-                    </div>
-                    <div className='space-y-5 mt-5'> 
-                      {column.perks && column.perks.map((perk, index) => {
+                  <div className='w-full h-full flex flex-col items-center justify-between gap-y-2'>
+                    <div className='price-content'>
+                      <div className='space-y-5'>
+                        {column.title && (<p className={`text-3xl font-medium bg-gradient-to-b ${last ? 'from-[#9c83fe]' : 'from-[#d6d5d5]'} ${last ? 'to-[#f6f5f5]' : 'to-white'} text-transparent bg-clip-text`}>{column.title}</p>)}
+                        {column.price && (<h2 className={`text-7xl font-semibold bg-gradient-to-b ${last ? 'from-[#9c83fe]' : 'from-[#d6d5d5]'} ${last ? 'to-[#f6f5f5]' : 'to-white'} ${inter.className} text-transparent bg-clip-text`}>${column.price}</h2>)}
+                        {column.offer && (<p>{column.offer}</p>)}
+                      </div>
+                      <div className='space-y-5 mt-5'> 
+                        {column.perks && column.perks.map((perk, index) => {
 
-                        return (
-                          <div key={index} className='flex gap-x-2 text-white'>
-                            <div className='border border-[#8669ff] rounded-md p-1 flex justify-center items-center'>
-                              <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5 13l4 4L19 7" />
-                              </svg>
+                          return (
+                            <div key={index} className='flex gap-x-2 text-white'>
+                              <div className='border border-[#8669ff] rounded-md p-1 flex justify-center items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5 13l4 4L19 7" />
+                                </svg>
+                              </div>
+                              <p>{perk}</p>
                             </div>
-                            <p>{perk}</p>
-                          </div>
-                        )
-                      })}
+                          )
+                        })}
+                      </div>
                     </div>
-                    {column.payment && (
-                      <div className='mt-16 flex justify-center md:justify-start'>
-                        <Link href={column.payment}>
-                          <Button variant={`${last ? 'gradient' : 'secondary'}`}>
-                            Get Started
-                          </Button>
-                        </Link>
-                      </div>
-                    )}
-                    {column.book && (
-                      <div className='mt-5 text-gray-300 hover:text-white flex justify-center md:justify-start underline'>
-                        <Link href={column.book}>
-                          Book a Call
-                        </Link>
-                      </div>
-                    )}
+                    <div className='price-payment flex flex-col items-center'>
+                      {column.payment && (
+                        <div className='mt-16 flex justify-center md:justify-start'>
+                          <Link href={column.payment}>
+                            <Button variant={`${last ? 'gradient' : 'secondary'}`}>
+                              Get Started
+                            </Button>
+                          </Link>
+                        </div>
+                      )}
+                      {column.book && (
+                        <div className='mt-5 text-gray-300 hover:text-white flex justify-center md:justify-start underline'>
+                          <Link href={column.book}>
+                            Book a Call
+                          </Link>
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
               )
